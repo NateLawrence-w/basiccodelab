3fdac1c959a687d3e54071fa0f7602febc464d02
NateLawrence-w <Natewolf676@gmail.com>
2024-09-11 10:11:48 -0400

    new upload

--- mycode.py
+++ mycode.py
@@ -3,3 +3,11 @@ def hello_world():
 
 if __name__ == "__main__":
     hello_world()
+
+j = input("welcome to the jungle, what is your Name?: ")
+print(j)
+
+if (j == "ninja"):
+    result = "welcom sir to the fog cloud"
+else:
+    print("welcome to the the jungle grab your swords and cameras and lets began out journey")
